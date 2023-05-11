8561827e77e06577c16e072d811cf2c9cb8df57f
ashly922 <ashly922@yahoo.com>
2023-05-10 20:59:00 -0700

    content correction

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-from 0-add import add
+from add_0 import add
 
 a = 1
 b = 2
-result = add(a,b)
 
-    print("{} + {} = {}".format(a, b, result))
+if __name__ == "__main__":
+    print(f"{a} + {b} = {add(a,b)}")
