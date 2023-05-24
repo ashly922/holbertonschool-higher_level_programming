3aaf3dcc1c17ee91591d71ec5343c1b8c3d784f5
ashly922 <ashly922@yahoo.com>
2023-05-23 19:12:43 -0700

    file moved

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """
-This module provides a method that adds two integers    
+This module provides a method that adds two integers
 """
+
+
 def add_integer(a, b=98):
     """
     Adds two integers and returns the sum
